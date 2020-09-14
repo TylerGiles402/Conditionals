ae7e1962ccda3c38ad5b6d84676ba0bfbb88b8af
TylerGiles402 <60163396+TylerGiles402@users.noreply.github.com>
2020-09-13 20:50:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var wrapperEle = document.body.querySelector(".wrapper");
+var checkEle = document.body.querySelector(".check");
+
+var prompter = prompt("What is the number?");
+
+var number1=15;
+
+var secondnumber = Number(prompt("What is the second number?"));
+
+var number2=10;
+var answer=(number1+number2)
+document.body.querySelector(".wrapper").innerHTML=answer;
